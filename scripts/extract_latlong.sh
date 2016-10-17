2307bdcba7cc81d25ce85182ee418a8a93556e09
akashiaallen <akashia@unm.edu>
2016-10-17 16:31:53 -0600

    script updates

--- scripts/extract_latlong.sh
+++ scripts/extract_latlong.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
         rm latlong.txt
-        for i in $( ls ../Centroids ); do 
-        	COORDINATE=`ogrinfo ../Centroids/$i $i |grep 'POINT'`
+        for i in $( ls ../JSON/Centroids ); do 
+        	COORDINATE=`ogrinfo -al ../JSON/Centroids/$i |grep 'POINT'`
             echo $i, $COORDINATE >> latlong.txt
             
         done
